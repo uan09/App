0a787e218689f3c11b0046a83d515e735581cd70
uan09 <uancoronel@gmail.com>
2023-06-17 03:51:13 +0800

    Displayed Processors in building PC. FINAL

--- app/src/main/java/com/example/app/CentralProcessingUnitActivity.java
+++ app/src/main/java/com/example/app/CentralProcessingUnitActivity.java
@@ -12,8 +12,6 @@ import com.example.app.ui.adapters.CpuAdapter;
 import com.example.app.ui.models.CpuModel;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,16 +20,15 @@ public class CentralProcessingUnitActivity extends AppCompatActivity {
 
     private static final String TAG = "CPUActivity";
     private FirebaseFirestore firestore;
-    private String email;
     private ArrayList<CpuModel> cpuModels;
-    String product_name, product_image, product_price, CPU_Cores, CPU_Socket, preferenceCategory;
+    String preferenceCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_central_processing_unit);
 
-        email = getIntent().getStringExtra("email");
+        String email = getIntent().getStringExtra("email");
         if (email != null) {
             Toast.makeText(CentralProcessingUnitActivity.this, "email" + email, Toast.LENGTH_SHORT).show();
         }
@@ -42,6 +39,7 @@ public class CentralProcessingUnitActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.cpu_recyclerview_view);
         CpuAdapter adapter = new CpuAdapter(this, cpuModels);
         recyclerView.setAdapter(adapter);
+        recyclerView.setNestedScrollingEnabled(false);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         firestore.collection("UserAccounts")
@@ -65,53 +63,51 @@ public class CentralProcessingUnitActivity extends AppCompatActivity {
                                         preferenceCategory = preferenceDocument.getString("preference_category");
                                         if (preferenceCategory != null) {
                                             // Query Firestore to fetch the desired products
-                                            Query query = firestore.collection("Products")
+                                            firestore.collection("Products")
                                                     .whereEqualTo("product_type", "Processor")
-                                                    .whereEqualTo("product_category", preferenceCategory);
+                                                    .whereEqualTo("product_category", preferenceCategory)
+                                                    .get()
+                                                    .addOnCompleteListener(productTask -> {
+                                                        if (productTask.isSuccessful() && !productTask.getResult().isEmpty()) {
+                                                            cpuModels.clear();
+                                                            for (DocumentSnapshot productDocument : productTask.getResult()) {
+                                                                String product_name = productDocument.getString("product_name");
+                                                                String product_price = productDocument.getString("product_price");
 
-                                            query.get().addOnCompleteListener(productTask -> {
-                                                if (productTask.isSuccessful()) {
-                                                    cpuModels.clear();
-                                                    for (QueryDocumentSnapshot productDocument : productTask.getResult()) {
-                                                        // Extract the necessary product information from the document
-                                                        product_name = productDocument.getString("product_name");
-                                                        product_price = productDocument.getString("product_price");
+                                                                List<String> productImages = (List<String>) productDocument.get("product_image");
+                                                                String product_image = null;
+                                                                if (productImages != null && !productImages.isEmpty()) {
+                                                                    product_image = productImages.get(0);
+                                                                }
 
-                                                        List<String> productImages = (List<String>) productDocument.get("product_image");
-                                                        if (productImages != null && !productImages.isEmpty()) {
-                                                            // Retrieve the first element of the array
-                                                            product_image = productImages.get(0);
+                                                                final String finalProductImage = product_image; // Declare final variable for product_image
+
+                                                                firestore.collection("Processor_DB")
+                                                                        .document(product_name)
+                                                                        .get()
+                                                                        .addOnCompleteListener(processorTask -> {
+                                                                            if (processorTask.isSuccessful()) {
+                                                                                DocumentSnapshot processorDocument = processorTask.getResult();
+                                                                                if (processorDocument.exists()) {
+                                                                                    String CPU_Cores = processorDocument.getString("CPU_Cores");
+                                                                                    String CPU_Socket = processorDocument.getString("CPU_Socket");
+
+                                                                                    CpuModel cpuModel = new CpuModel(product_name, finalProductImage, product_price, CPU_Cores, CPU_Socket);
+                                                                                    cpuModels.add(cpuModel);
+
+                                                                                    if (cpuModels.size() == productTask.getResult().size()) {
+                                                                                        adapter.notifyDataSetChanged();
+                                                                                    }
+                                                                                }
+                                                                            } else {
+                                                                                Log.d(TAG, "Error getting processor document: ", processorTask.getException());
+                                                                            }
+                                                                        });
+                                                            }
+                                                        } else {
+                                                            Log.d(TAG, "Error getting products: ", productTask.getException());
                                                         }
-
-                                                        // Query the Processor_DB collection based on product_name
-                                                        firestore.collection("Processor_DB")
-                                                                .document(product_name)
-                                                                .get()
-                                                                .addOnCompleteListener(processorTask -> {
-                                                                    if (processorTask.isSuccessful()) {
-                                                                        DocumentSnapshot processorDocument = processorTask.getResult();
-                                                                        if (processorDocument.exists()) {
-                                                                            // Retrieve the CPU_Cores and CPU_Socket fields
-                                                                            CPU_Cores = processorDocument.getString("CPU_Cores");
-                                                                            CPU_Socket = processorDocument.getString("CPU_Socket");
-
-                                                                            // Create a new CpuModel object with the extracted information
-                                                                            CpuModel cpuModel = new CpuModel(product_name, product_image, product_price, CPU_Cores, CPU_Socket);
-                                                                            cpuModels.add(cpuModel);
-                                                                        }
-                                                                    } else {
-                                                                        Log.d(TAG, "Error getting processor document: ", processorTask.getException());
-                                                                    }
-
-                                                                    if (cpuModels.size() == productTask.getResult().size()) {
-                                                                        adapter.notifyDataSetChanged();
-                                                                    }// Notify the adapter about the updated data
-                                                                });
-                                                    }
-                                                } else {
-                                                    Log.d(TAG, "Error getting products: ", productTask.getException());
-                                                }
-                                            });
+                                                    });
                                         } else {
                                             Log.d(TAG, "User preference category is null");
                                         }
@@ -123,5 +119,6 @@ public class CentralProcessingUnitActivity extends AppCompatActivity {
                         Log.d(TAG, "Error getting user account: ", task.getException());
                     }
                 });
-        }
+
+    }
     }
