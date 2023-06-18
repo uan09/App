2c8531d2f4ecdfbf9ecb259db7819da8ce61479e
uan09 <uancoronel@gmail.com>
2023-06-18 16:02:23 +0800

    finished motherboard and gpu display. RAM work in progress.

--- app/src/main/java/com/example/app/RandomAccessMemoryActivity.java
+++ app/src/main/java/com/example/app/RandomAccessMemoryActivity.java
@@ -47,62 +47,69 @@ public class RandomAccessMemoryActivity extends AppCompatActivity {
                     if (tempDocumentSnapshot.exists()) {
                         String motherboardMemoryType = tempDocumentSnapshot.getString("Motherboard_Memory_Type");
 
-                        // Get all documents in the RAM_DB collection where Memory_Type is equal to Motherboard_Memory_Type
+                        // Get all documents in the GPU_DB collection with GPU_Memory_Type equal to motherboardMemoryType
                         firestore.collection("RAM_DB")
                                 .whereEqualTo("Memory_Type", motherboardMemoryType)
                                 .get()
-                                .addOnSuccessListener(ramQuerySnapshot -> {
+                                .addOnSuccessListener(gpuDocumentSnapshots -> {
                                     List<Task<QuerySnapshot>> tasks = new ArrayList<>();
-                                    for (DocumentSnapshot ramDocument : ramQuerySnapshot.getDocuments()) {
-                                        String memoryName = ramDocument.getString("Memory_Name");
+                                    for (DocumentSnapshot ramDocumentSnapshot : gpuDocumentSnapshots) {
+                                        String ramId = ramDocumentSnapshot.getId();
 
-                                        // Get all products from the Products collection where product_name equals RAM_DB document ID
                                         Task<QuerySnapshot> task = firestore.collection("Products")
-                                                .whereEqualTo("product_name", memoryName)
+                                                .whereEqualTo("product_name", ramId)
                                                 .whereEqualTo("product_type", "Random Access Memory")
-                                                .whereEqualTo("store_name", "BitoyPc")
-                                                .get()
-                                                .addOnSuccessListener(productQuerySnapshot -> {
-                                                    for (DocumentSnapshot productDocument : productQuerySnapshot.getDocuments()) {
-                                                        String memoryType = ramDocument.getString("Memory_Type");
-                                                        String memoryCapacity = ramDocument.getString("Memory_Capacity");
-                                                        String productPrice = productDocument.getString("product_price");
+                                                .get();
+                                        tasks.add(task);
+                                    }
 
-                                                        List<String> productImages = (List<String>) productDocument.get("product_image");
+                                    // Wait for all the queries to complete
+                                    Task<List<QuerySnapshot>> combinedTask = Tasks.whenAllSuccess(tasks);
+                                    combinedTask.addOnSuccessListener(querySnapshotsList -> {
+                                        ramModels.clear(); // Clear the previous data
+                                        for (QuerySnapshot querySnapshot : querySnapshotsList) {
+                                            if (!querySnapshot.isEmpty()) {
+                                                for (DocumentSnapshot productDocumentSnapshot : querySnapshot.getDocuments()) {
+                                                    String memoryName = productDocumentSnapshot.getString("product_name");
+
+                                                    // Retrieve GPU_Memory_Type and GPU_Chipset
+                                                    DocumentSnapshot gpuDocumentSnapshot = gpuDocumentSnapshots.getDocuments().stream()
+                                                            .filter(doc -> doc.getId().equals(memoryName))
+                                                            .findFirst()
+                                                            .orElse(null);
+                                                    if (gpuDocumentSnapshot != null) {
+                                                        String memoryType = gpuDocumentSnapshot.getString("Memory_Type");
+                                                        String memoryCapacity = gpuDocumentSnapshot.getString("Memory_Capacity");
+
+                                                        String productPrice = productDocumentSnapshot.getString("product_price");
+                                                        List<String> productImages = (List<String>) productDocumentSnapshot.get("product_image");
                                                         String productImage = null;
                                                         if (productImages != null && !productImages.isEmpty()) {
                                                             productImage = productImages.get(0);
                                                         }
 
-                                                        // Create RamModel instance and add it to the ArrayList
-                                                        RamModel ramModel = new RamModel(memoryName, memoryType, memoryCapacity, productImage, productPrice);
+                                                        RamModel ramModel = new RamModel( memoryName, memoryType, memoryCapacity, productImage, productPrice);
                                                         ramModels.add(ramModel);
                                                     }
-
-                                                    // Notify the adapter about the data change
-                                                    adapter.notifyDataSetChanged();
-                                                })
-                                                .addOnFailureListener(e -> {
-                                                    // Handle any errors that occurred while fetching the product documents
-                                                });
-
-                                        tasks.add(task);
-                                    }
-
-                                    // Wait for all tasks to complete before notifying the adapter
-                                    Tasks.whenAllComplete(tasks)
-                                            .addOnCompleteListener(task -> {
-                                                // Notify the adapter about the data change
-                                                adapter.notifyDataSetChanged();
-                                            });
+                                                }
+                                            }
+                                        }
+                                        adapter.notifyDataSetChanged();
+                                        // Check if the list is empty and handle accordingly
+                                    });
+                                    combinedTask.addOnFailureListener(e -> {
+                                        // Handle any errors that occurred while fetching products from the Products collection
+                                    });
                                 })
                                 .addOnFailureListener(e -> {
-                                    // Handle any errors that occurred while fetching the RAM_DB documents
+                                    // Handle any errors that occurred while querying the GPU_DB collection
                                 });
+                    } else {
+                        // Handle the case when the Temp document does not exist
                     }
                 })
                 .addOnFailureListener(e -> {
-                    // Handle any errors that occurred while fetching the Temp document
+                    // Handle any errors that occurred while reading the Temp document
                 });
     }
 
